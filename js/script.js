58dffde81a61632466b7b9feaf439faa5af7a3e2
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-03-29 15:31:31 +0200

    js: dadi

--- js/script.js
+++ js/script.js
@@ -16,4 +16,18 @@ if(emailValida){
     console.log("Accesso negato")
 }
 
-//console.log(validEmail[1])
+
+
+let numeroUno = Math.floor(Math.random() * 6);
+let numeroDue = Math.floor(Math.random() * 6);
+
+console.log(numeroUno)
+console.log(numeroDue)
+
+if(numeroUno > numeroDue){
+    console.log("Ha vinto l'essere umano")
+} else if (numeroUno < numeroDue){
+    console.log("Ha vinto il computer")
+} else{
+    console.log("Pareggio")
+}
