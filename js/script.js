94042f4b753b709d8aa16c0971ee34225e3cf225
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-03-30 09:51:34 +0200

    js: aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+//Mail
 let bottone = document.getElementById("calcola")
 let mail = document.getElementById("mail")
 let risultato = document.getElementById("risultato")
 
 bottone.addEventListener("click", function(){
+
     risultato.classList.remove("d-none")
     let validEmail = ["prova0@gmail.com", "prova1@gmail.com", "prova2@gmail.com"];
     let emailValida = false;
@@ -14,15 +16,12 @@ bottone.addEventListener("click", function(){
     }
     if(emailValida){
         risultato.innerHTML = "Accesso eseguito";
-        
     } else {
         risultato.innerHTML = "Accesso negato. Usa e-mail valida";
     }
 })
 
-
-
-
+//Gioco dei dadi
 let bottone2 = document.getElementById("genera")
 let generato1 = document.getElementById("numero1")
 let generato2 = document.getElementById("numero2")
@@ -30,16 +29,12 @@ let risultato2 = document.getElementById("risultato2")
 let giocatore1 = document.getElementById("giocatore1")
 let giocatore2 = document.getElementById("giocatore2")
 
-
 bottone2.addEventListener("click", function(){
+
     generato1.classList.remove("d-none");
-
     generato2.classList.remove("d-none");
-
     risultato2.classList.remove("d-none");
-
     giocatore1.classList.remove("d-none");
-
     giocatore2.classList.remove("d-none");
 
     let numero1 = Math.floor(Math.random() * 6);
@@ -50,7 +45,6 @@ bottone2.addEventListener("click", function(){
         risultato2.innerHTML = ("Ha vinto l'essere umano")
         generato1.innerHTML = numero1
         generato2.innerHTML = numero2
-
     } 
     else if (numero1 < numero2){
         risultato2.innerHTML = ("Ha vinto il computer")
@@ -60,6 +54,4 @@ bottone2.addEventListener("click", function(){
     else{
         ("Pareggio")
     }
-
-
 })
